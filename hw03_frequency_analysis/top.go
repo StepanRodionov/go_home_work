db0a20325768546f88eba3644542f8555fbec589
Stepan <stepan@C02CN9ANMD6N.local>
2024-04-13 12:33:24 +0300

    Frequency task

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	topFrequencies := make(map[string]int)
+	for _, word := range words {
+		if word == "" {
+			continue
+		}
+		if _, ok := topFrequencies[word]; ok {
+			topFrequencies[word]++
+		} else {
+			topFrequencies[word] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(topFrequencies))
+	for k := range topFrequencies {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		if topFrequencies[keys[i]] == topFrequencies[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return topFrequencies[keys[i]] > topFrequencies[keys[j]]
+	})
+
+	sorted := make([]string, 0, 10)
+	for i, word := range keys {
+		if i == 10 {
+			break
+		}
+		sorted = append(sorted, word)
+	}
+	return sorted
 }
