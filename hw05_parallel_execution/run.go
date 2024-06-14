431581a97d0a55de330c29a0914eb716c4bb981f
Stepan <stepan@C02CN9ANMD6N.local>
2024-06-14 19:44:11 +0300

    bad solution

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,6 +10,38 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	activeJobs := 0
+	i := 0
+	errs := 0
+	taskLen := len(tasks)
+	tasksChan := make(chan int, n)
+	for {
+		if i >= taskLen {
+			break
+		}
+		if activeJobs < n {
+			if errs >= m {
+				return ErrErrorsLimitExceeded
+			}
+			task := tasks[i]
+			go func() {
+				defer func() {
+					tasksChan <- 0
+				}()
+				err := doOneJob(task)
+				if err != nil {
+					errs++
+				}
+			}()
+			activeJobs++
+			i++
+		}
+		<-tasksChan
+		activeJobs--
+	}
 	return nil
+}
+
+func doOneJob(fn Task) error {
+	return fn()
 }
